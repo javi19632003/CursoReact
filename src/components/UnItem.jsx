0e32b9a0baa77208dd6fa65d1d75a11962a4b45c
javi19632003 <anicolas@transclor.com.ar>
2021-09-04 16:17:02 -0300

    firebase I

--- src/components/UnItem.jsx
+++ src/components/UnItem.jsx
@@ -6,11 +6,11 @@ export default function UnItem ({desc = ' ', preuni = 0, caract = ' ', stk = 0, 
         <>
          <div className='impitem'>
             <img className='img-fluid' src= { '.'+urlfoto } alt=""></img>
-            </div>
-            <div> { desc } </div>
-            <div> { preuni } </div>
-            <div> { caract } </div>
-            <div> { stk } </div>
+         </div>
+         <div> { desc } </div>
+         <div> { preuni } </div>
+         <div> { caract } </div>
+         <div> { stk } </div>
         </>
     )
 
--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,4 +1,14 @@
+
+import React, { useContext } from 'react';
+import { Context } from "../context/CartContext";
+
 export default function CartItem ({id, desc, cant, preuni}) {
+
+    const datoContexto = useContext(Context)
+    
+    const BorrarCart = (id) => {
+        datoContexto.removeItem(id)
+    }
 
     return(
         <>
@@ -7,6 +17,8 @@ export default function CartItem ({id, desc, cant, preuni}) {
                 <p className='cartitem1'>{cant}</p>
                 <p className='cartitem2'>{desc}</p>
                 <p className='cartitem1'>{preuni}</p>
+                <button onClick={()=> BorrarCart(id)} >Borrar</button>
+
             </div>
         </>
 
